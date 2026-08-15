0f5e3fa17a88d96c2fbc307962475fce2f07dff2
Rafi <rafiahamed2021@gmail.com>
2026-08-16 01:00:49 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const themeToggle = document.querySelector('.theme-toggle');
 const menuButton = document.querySelector('.menu-button');
 const mobileMenu = document.querySelector('.mobile-menu');
-loaderDuration = 3250;
+const loaderDuration = 3100;
 
 function applyTheme(theme) {
   const isLight = theme === 'light';
